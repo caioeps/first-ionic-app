fbdccea00a5d3812c3e11b72a1fa935f2c7294da
caio ergos <caioergos@gmail.com>
2017-06-14 19:16:32 -0300

    still doing it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+
 import { LoginPage } from '../pages/login/login';
+import { ChatPage } from '../pages/chat/chat';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,24 +15,19 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage
+    LoginPage,
+    ChatPage,
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage
+    LoginPage,
+    ChatPage,
   ],
   providers: [
     StatusBar,
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { Socket, Presence } from 'phoenix';
+
+import { ChatPage } from '../chat/chat';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  url: string;
+  user = { username: '', socket: null };
+  headers: Headers;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl : AlertController, public http: Http, public platform:Platform) {
+    this.headers = new Headers();
+    this.headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    this.url = this.platform.is('cordova') ? 'http://localhost:4000/api' : '/api';
   }
 
+  login() {
+    console.log(this.user);
+    console.log(this.url);
+
+    this.http.post(this.url + '/users?user[username]=' + this.user.username, {}, { headers: this.headers })
+      .map(res => res.json)
+      .subscribe(
+        res => {
+          console.log(res);
+          console.log('Logged in!');
+          this.user.socket = new Socket(this.url + '/socket', {
+            params: { user: this.user }
+          });
+          this.user.socket.connect()
+          this.navCtrl.push(ChatPage);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+  }
 }
